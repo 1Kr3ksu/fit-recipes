5b84247bca18cf82bea84652d54751395cc83fed
1Kr3ksu <bielawskowski@gmail.com>
2025-10-23 10:18:49 +0200

    struktura projektu

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
- 
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>🥗 Fit Recipes</h1>
+      <p>Aplikacja w budowie...</p>
+    </div>
+  )
 }
 
 export default App
